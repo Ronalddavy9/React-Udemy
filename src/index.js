563144ebc3a8d08c0fb0c89fcbc941ddd9191c25
Ronald Davy <ronalddavy9@gmail.com>
2023-01-02 22:49:36 -0300

    Primeiro commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+import ReactDOm from 'react-dom'
+import React from 'react'
+
+import Primeiro from './componetes/basicos/Primeiro'
+
+
+ReactDOm.render(
+    <div>
+        <Primeiro>
+    </div>,
+    document.getElementById('root')
+)
